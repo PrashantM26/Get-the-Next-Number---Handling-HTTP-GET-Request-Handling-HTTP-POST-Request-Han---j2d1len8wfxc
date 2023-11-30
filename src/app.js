cfff3947304b83b9db0545460e2c64edea700bd5
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-11-30 18:24:13 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.get('/api/get-next-num', (req, res) => {
    const num = req.body.num;
 
-  /*if (!num || isNaN(num) || num == 0) {
+  /*if (!num || isNaN(num)) {
     return res.status(400).json({
       status: 'failure',
       message: 'Invalid number. Please provide a valid number in the request body.',
